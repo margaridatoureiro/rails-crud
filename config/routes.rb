7308fb4a60d999ea1176217c9fed2d66884abc63
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-08-15 11:21:44 +0100

    rails crud app completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # verb "path", to: "controller#action"
+  # CRUD (create, read, update, destroy)
+  # See all restaurants
+  # get "restaurants", to: "restaurants#index"
+  # # Create a restaurant
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant  # see the form
+  # post "restaurants", to: "restaurants#create" # submit the form
+  # # See one restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # # Update a restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant # see the form
+  # patch "restaurants/:id", to: "restaurants#update" # submit the form
+  # # Destroy a restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
